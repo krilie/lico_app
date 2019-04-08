dde66b3515545221557538ca189bfb6e5a158ffe
i <776334655@qq.com>
2019-04-08 19:52:30 +0800

    app init

---
+++ lib/widget/account/account_login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// login weight
+/// check login status and login with name password
+class AccountLoginWidget extends StatelessWidget {
+
+  final String preToken;
+
+  AccountLoginWidget({Key key, this.preToken}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("ok");
+  }
+}
+
