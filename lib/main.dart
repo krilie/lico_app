7f0f7afe190fa1508c85c97cde9454b01b725420
acorn <1197829331@qq.com>
2019-05-21 22:26:22 +0800

    window

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lico_app/splash_page/splash.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
