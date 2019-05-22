4db0b8288453bd796f4f3e95144d90dbb71fe64e
lhh <776334655@qq.com>
2019-05-22 14:07:01 +0800

    network vo

--- lib/vo/user.dart
+++ lib/vo/user.dart
@@ -1,5 +1,9 @@
 import 'package:json_annotation/json_annotation.dart';
 
+// user.g.dart 将在我们运行生成命令后自动生成
+part 'user.g.dart';
+
+///这个标注是告诉生成器，这个类是需要生成Model类的
 @JsonSerializable()
 class UserInfo {
   String userName;
