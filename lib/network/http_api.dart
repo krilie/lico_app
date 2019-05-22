c93c09e7aa139d9977b725fa72f5803c741f261e
lhh <776334655@qq.com>
2019-05-22 15:59:12 +0800

    to json

--- lib/network/http_api.dart
+++ lib/network/http_api.dart
@@ -1,18 +1,49 @@
 import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-// or new Dio with a BaseOptions instance.
-BaseOptions options = new BaseOptions(
-  baseUrl: "https://api.ligo.ml",
-  connectTimeout: 5000,
-  receiveTimeout: 3000,
-);
-Dio ligoDio = new Dio(options);
+part 'http_api.g.dart';
 
-void getHttp() async {
-  try {
-    Response response = await Dio().get("http://www.google.com");
-    print(response);
-  } catch (e) {
-    print(e);
+// =================================================================
+@JsonSerializable()
+class UserLoginRet {
+  String token;
+  UserLoginRet(this.token);
+  factory UserLoginRet.fromJson(Map<String,dynamic> json) => _$UserLoginRetFromJson(json);
+  Map<String,dynamic> toJson()=>_$UserLoginRetToJson(this);
+}
+
+class api {
+  // 工厂模式
+  factory api() =>_getInstance();
+  static api get instance => _getInstance();
+  static api _instance = new api._internal();
+  static api _getInstance() {
+    return _instance;
   }
+
+  // or new Dio with a BaseOptions instance.
+  BaseOptions options;
+  Dio dio;
+  api._internal() {
+    options = new BaseOptions(
+      baseUrl: "https://api.ligo.ml",
+      connectTimeout: 5000,
+      receiveTimeout: 3000,
+    );
+    dio = new Dio(options);
+  }
+  void setErrorInterceptor(Interceptor int){
+    dio.interceptors.add(int);
+  }
+  // login
+  Future<UserLoginRet> Userlogin(String userName,String password) async {
+    try {
+      Response response = await dio.post("/login",data: {"":""});
+      print(response);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
 }
---
+++ lib/network/http_api.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'http_api.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserLoginRet _$UserLoginRetFromJson(Map<String, dynamic> json) {
+  return UserLoginRet(json['token'] as String);
+}
+
+Map<String, dynamic> _$UserLoginRetToJson(UserLoginRet instance) =>
+    <String, dynamic>{'token': instance.token};
