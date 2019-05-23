05b8988473b10e50ec69cde75ae8d8c75978e755
lhh <776334655@qq.com>
2019-05-23 10:26:08 +0800

    to json

--- lib/splash_page/splash.dart
+++ lib/splash_page/splash.dart
@@ -2,16 +2,14 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:lico_app/HomePage.dart';
 
-class SplashPage extends StatefulWidget{
+class SplashPage extends StatefulWidget {
+  SplashPage({Key key}) : super(key: key);
 
-  SplashPage({Key key}):super(key:key);
   @override
-  _SplashPage createState()=> new _SplashPage();
-
+  _SplashPage createState() => new _SplashPage();
 }
 
-class _SplashPage extends State<SplashPage>{
-
+class _SplashPage extends State<SplashPage> {
   bool isStartHomePage = false;
 
   @override
@@ -25,27 +23,21 @@ class _SplashPage extends State<SplashPage>{
     );
   }
 
-
   //页面初始化状态的方法
   @override
   void initState() {
     super.initState();
-    //开启倒计时
-    countDown();
-  }
-
-  void countDown() {
     //设置倒计时三秒后执行跳转方法
     var duration = new Duration(seconds: 3);
-    new Future.delayed(duration, goToHomePage);
-  }
-
-  void goToHomePage(){
-    //如果页面还未跳转过则跳转页面
-    if(!isStartHomePage){
-      //跳转主页 且销毁当前页面
-      Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (context)=>new HomePage()), (Route<dynamic> rout)=>false);
-      isStartHomePage=true;
-    }
+    new Future.delayed(duration, () {
+      //如果页面还未跳转过则跳转页面
+      if (!isStartHomePage) {
+        //跳转主页 且销毁当前页面
+        Navigator.of(context).pushAndRemoveUntil(
+            new MaterialPageRoute(builder: (context) => new HomePage()),
+            (Route<dynamic> rout) => false);
+        isStartHomePage = true;
+      }
+    });
   }
 }
