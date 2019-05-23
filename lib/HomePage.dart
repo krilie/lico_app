89d20d8453be772cf2c3fcaf976f5d7177d82fb6
lhh <776334655@qq.com>
2019-05-23 10:00:02 +0800

    to json

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -16,25 +16,19 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text('应用'),
-      ),
-      body: FlatButton(
-          onPressed: () {
-            var token = api.instance.userLogin("aa", "bb");
-            token.then((o) {
-              debugPrint(o.toString());
-            }).catchError((e){
-              var msg = StdRet.fromJson(e.response.data);
-              var snackBar = new SnackBar(
-                  content: new Text(
-                      e.response.statusCode.toString() + msg.code + msg.message));
-              Scaffold.of(context).showSnackBar(snackBar);
-              return e.response;
-            });
-          },
-          child: Text("ok")),
-    );
+        appBar: new AppBar(
+          title: new Text('应用'),
+        ),
+        body: Builder(builder: (BuildContext context) {
+          return FlatButton(
+              onPressed: () {
+                var token = api.instance.userLogin("aa", "bb");
+                token.then((o) {
+                  debugPrint(o.toString());
+                });
+              },
+              child: Text("ok"));
+        }));
   }
 
   @override
