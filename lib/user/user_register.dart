5e3deba2d34cc544f94823de73f3c6d6e680f2d9
lhh <776334655@qq.com>
2019-05-24 11:27:21 +0800

    注册页面

--- lib/user/user_register.dart
+++ lib/user/user_register.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 
 class UserRegisterScreen extends StatefulWidget {
   @override
-  _UserRegisterScreen createState() => new _UserRegisterScreen();
+  _UserRegisterScreen createState() => _UserRegisterScreen();
 }
 
 class _UserRegisterScreen extends State<UserRegisterScreen> {
-  TextEditingController userLoginNameController = TextEditingController(); // 用户名
+  TextEditingController userLoginNameController = TextEditingController(); // 用
   TextEditingController passwordController = TextEditingController(); // 密码
   FocusNode secondTextFieldNode = FocusNode(); // 光标
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("注册")),
-      body: new Padding(
-          padding: new EdgeInsets.all(8.0),
-          child: Card(child: const Text('Hello World!')),
+      appBar: AppBar(centerTitle: true, title: Text("注册")),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Center(
+          widthFactor: 180,
+          heightFactor: 80,
+          child: Text("center"),
+        ),
       ),
     );
   }
-
 }
